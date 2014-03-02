dc24bb56d1a587649c6a4597b098334ebb2f1003
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-03-01 18:04:56 -0600

    utilities

--- examples/common/utilities.js
+++ examples/common/utilities.js
@@ -1,29 +1,19 @@
 // Copyright (C) 2014 Erik Ringsmuth <erik.ringsmuth@gmail.com>
 define([
-  'jquery',
   'prettify'
-], function($, prettify) {
+], function(prettify) {
   'use strict';
 
   var utilities = {
     // Format code for readability. This will look for every code block in the element and format it.
     formatCode: function formatCode(element) {
-      $(element).find('code').each(function() {
-        var $el = $(this);
-        // If the code block has class 'pln' don't format the code
-        if (!$el.hasClass('pln')) {
-          $el.html(prettify.prettyPrintOne($el.html()));
+      var elements = element.querySelectorAll('pre');
+      for (var el in elements) {
+        if (elements.hasOwnProperty(el)) {
+          elements[el].innerHTML = prettify.prettyPrintOne(elements[el].innerHTML);
         }
-      });
+      }
       return element;
-    },
-
-    // GUID generator
-    guid: function guid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-      });
     },
 
     // Escape text being added to the DOM
